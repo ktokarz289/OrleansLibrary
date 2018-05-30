31eac9c168802179e6fc9e77c7f47835747b4053
= <kj.tokarz@gmail.com>
2018-05-29 20:17:40 -0400

    Write function to list books for user
    Write function to list user's books
    Create list to hold books
    Create grain interface

--- OrleansGrainInterfaces/ICustomerGrain.cs
+++ OrleansGrainInterfaces/ICustomerGrain.cs
@@ -1,8 +1,11 @@
 ﻿using Orleans;
+using System.Threading.Tasks;
 
 namespace OrleansGrainInterfaces
 {
     public interface ICustomerGrain : IGrainWithIntegerKey
     {
+        Task<string> GetBooks();
+        Task CheckoutBook(string name);
     }
 }
