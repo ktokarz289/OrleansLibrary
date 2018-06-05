fcbca27c4e13afd7a1d9bb7b62a2814b41040c52
= <kj.tokarz@gmail.com>
2018-06-04 20:00:27 -0400

    Set up client console project
    Add the code to connect the client to the silo.

---
+++ OrleansLibraryClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using Orleans;
+using Orleans.Configuration;
+using OrleansGrainInterfaces;
+using System;
+
+namespace OrleansLibraryClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ClientBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "development";
+                    options.ServiceId = "OrleansLibrary";
+                })
+                .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(ILibrarianGrain).Assembly).WithReferences())
+                .Build();
+
+            client.Connect().Wait();
+        }
+    }
+}
